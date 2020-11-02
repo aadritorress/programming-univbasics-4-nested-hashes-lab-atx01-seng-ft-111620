c9ffa2d36351ba4fe327a0aa0c51d8e0c39cb7bc
Adri Torres <adriana.amaral@msn.com>
2020-11-02 20:40:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,7 +60,7 @@ end
 
 def adding_matz
   
-  	programmer_hash [:yukihiro_matsumoto => {:languages => {}}]
+  	programmer_hash [:yukihiro_matsumoto => {:languages => {"LISP", }}]
   
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
